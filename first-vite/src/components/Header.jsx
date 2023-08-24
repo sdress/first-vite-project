eeebf2a78838399e86e6a9b511a808acc30d89f3
sdress <sarah.dresser1@gmail.com>
2023-08-24 12:49:48 +0200

    saving components folder

---
+++ first-vite/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div id="header">
+            <h1 className='title'>Johannesen & Sons Greenscaping</h1>
+            <h3>Save the planet (and your waterbill) with Johannesen Greenscaping</h3>
+        </div>
+    );
+}
+
+export default Header;
---
+++ first-vite/src/components/Project.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Project = (props) => {
+    const {image1, image2} = props;
+    return (
+        <div>
+            <p>(Placeholder for before and after photos)</p>
+        </div>
+    )
+};
+
+export default Project;
