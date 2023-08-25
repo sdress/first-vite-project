d92ddf3aca691bb958e6bf9cbe9482cf2df26191
sdress <sarah.dresser1@gmail.com>
2023-08-25 12:04:57 +0200

    saving new Contact component

--- first-vite/src/components/Project.jsx
+++ first-vite/src/components/Project.jsx
@@ -6,8 +6,8 @@ const Project = (props) => {
     return (
         <div id="image-slider">
         <ImgComparisonSlider>
-            <img slot="first" src="src/assets/before1.webp" />
-            <img slot="second" src="src/assets/after1edited.jpg" />
+            <img slot="first" src="src/assets/before1.webp" alt="An untended backyard area" />
+            <img slot="second" src="src/assets/after1edited.jpg" alt="A well-maintained backyard patio space" />
         </ImgComparisonSlider>
         </div>
     );
