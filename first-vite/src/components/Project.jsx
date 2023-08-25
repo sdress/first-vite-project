610aae80d8a4c806be250b9b4a27b2d33468ad83
sdress <sarah.dresser1@gmail.com>
2023-08-25 11:10:26 +0200

    saving place in styling

--- first-vite/src/components/Project.jsx
+++ first-vite/src/components/Project.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {Button, ButtonToggle} from "reactstrap";
 
 const Project = (props) => {
     const {image1, image2} = props;
     return (
         <div>
+            <div>
+                <Button color="outline-primary">
+                    Click to see Before and After
+                </Button>
+            </div>
             <p>(Placeholder for before and after photos)</p>
         </div>
     )
