4c80c5bf7c95c2d0a53355e33e815e1d513fb770
sdress <sarah.dresser1@gmail.com>
2023-08-25 11:37:19 +0200

    adding images and comparison slider

--- first-vite/src/components/Project.jsx
+++ first-vite/src/components/Project.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { ImgComparisonSlider } from '@img-comparison-slider/react';
 
 const Project = (props) => {
     const {image1, image2} = props;
     return (
-        <div>
-            <p>(Placeholder for before and after photos)</p>
+        <div id="image-slider">
+        <ImgComparisonSlider>
+            <img slot="first" src="src/assets/before1.webp" />
+            <img slot="second" src="src/assets/after1edited.jpg" />
+        </ImgComparisonSlider>
         </div>
-    )
+    );
 };
 
 export default Project;
