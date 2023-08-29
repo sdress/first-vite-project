cc5407152add6f6fea5d5047ede2a73871236579
sdress <sarah.dresser1@gmail.com>
2023-08-29 09:53:53 +0200

    saving renamed component Project -> ImgSlider

---
+++ first-vite/src/components/ImgSlider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ImgComparisonSlider } from '@img-comparison-slider/react';
+
+const ImgSlider = (props) => {
+    const {image1, image2} = props;
+    return (
+        <div id="image-slider">
+        <ImgComparisonSlider>
+            <img slot="first" src="src/assets/before1.webp" alt="An untended backyard area" />
+            <img slot="second" src="src/assets/after1edited.jpg" alt="A well-maintained backyard patio space" />
+        </ImgComparisonSlider>
+        </div>
+    );
+};
+
+export default ImgSlider;
