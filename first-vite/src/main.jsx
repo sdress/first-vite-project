9a21b6d09b48380e1e215b5a386d7b2c09d0c644
sdress <sarah.dresser1@gmail.com>
2023-08-29 11:08:09 +0200

    saving JS & CSS styling (header scroll & alignment)

--- first-vite/src/main.jsx
+++ first-vite/src/main.jsx
@@ -9,3 +9,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "1% 1%";
+    document.getElementById("name").style.fontSize = "2vh";
+  } else {
+    document.getElementById("navbar").style.padding = "5% 2%";
+    document.getElementById("name").style.fontSize = "4vh";
+  }
+}
