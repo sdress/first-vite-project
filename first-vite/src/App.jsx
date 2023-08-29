cf4510d05925b66fc8bb98a80bc748111888c7e2
sdress <sarah.dresser1@gmail.com>
2023-08-29 09:52:51 +0200

    saving renamed component and added Bootstrap carousel

--- first-vite/src/App.jsx
+++ first-vite/src/App.jsx
@@ -2,15 +2,39 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Project from './components/Project';
+// import Project from './components/ImgSlider';
 import Header from './components/Header';
+import ImgSlider from './components/ImgSlider';
 
 function App() {
 
   return (
     <div className='App'>
       <Header />
-      <Project />
+
+      <h1>Project Examples:</h1>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <ImgSlider />
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Second slide" />
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Third slide" />
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
     </div>
   );
 }
